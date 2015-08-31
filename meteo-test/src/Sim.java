ad04d30056539329a22307259e8bbc1f35d114df
EANDWRO <andrzej.wronka@ericsson.com>
2015-08-31 14:32:38 +0200

    simulation main class refactored.
    Handling keyboard input

--- meteo-test/src/Sim.java
+++ meteo-test/src/Sim.java
@@ -1,14 +1,34 @@
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Sim {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		final float fTemp = 30.5f;
 		WeatherData wdTest = new WeatherData();
 		WeatherForecaster printerTest = new WeatherForecaster(wdTest);
-		
-		wdTest.setWeatherState(1250.5f, 30.0f);
-		wdTest.setWeatherState(1000.5f, 30.0f);
-		
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Program do monitorowania zmian"
+				+ "cisnienia atmosferycznego\n"
+				+ "wprowadz \"exit\" lub \"close\" "
+				+ "by zakonczyc");
+
+		boolean bflag = true;
+		while (bflag) {
+			System.out.println("Odczytaj aktualne cisnienie: ");
+			String userInp = sc.next();
+			try {
+				wdTest.setWeatherState(Float.parseFloat(userInp), fTemp);
+			} catch (Exception e) {
+				if ((userInp.equals("exit")) || (userInp.equals("close"))) {
+					bflag = false;
+				}
+			}
+		}
+
+		System.out.println("Pomiar zakonczony.");
+
 	}
-
 }
-
